eea5fe09b617416d47aa0a673f4150699d9d47c8
김은조 <zmffheldk@gmail.com>
2022-02-25 02:01:26 +0900

    create UserRepository interface

---
+++ src/main/java/com/example/study/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.study.repository;
+
+import com.example.study.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {//user 기본키 id 의 타입이 Long
+}
---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplication;
+
+public class UserRepositoryTest extends StudyApplication {
+
+    public void create(){
+
+    }
+}
