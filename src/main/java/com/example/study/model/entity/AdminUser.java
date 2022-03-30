db7631009ce635dd8b1d8332726c750c7033d273
김은조 <zmffheldk@gmail.com>
2022-03-30 17:29:55 +0900

    create JPA Auditing

--- src/main/java/com/example/study/model/entity/AdminUser.java
+++ src/main/java/com/example/study/model/entity/AdminUser.java
@@ -1,5 +1,6 @@
 package com.example.study.model.entity;
 
+import javax.persistence.EntityListeners;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class AdminUser {
 
     @Id
@@ -36,9 +43,14 @@ public class AdminUser {
 
     private LocalDateTime registeredAt;
     private LocalDateTime unregisteredAt;
+
+    @CreatedDate
     private LocalDateTime createdAt;
+    @CreatedBy
     private String createdBy;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
+    @LastModifiedBy
     private String updatedBy;
 
 }
