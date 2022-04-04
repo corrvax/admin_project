e2e048ed7ba1f2969a19233e942850b8edddf0d3
김은조 <zmffheldk@gmail.com>
2022-04-05 01:54:12 +0900

    create UserApiController

---
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -0,0 +1,22 @@
+package com.example.study.controller.api;
+
+import com.example.study.model.network.Header;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController {
+
+//    public Header create(){     //C
+//
+//    }
+//
+//    public Header read(){       //R
+//
+//    }
+                                //U
+
+                                //D
+
+}
