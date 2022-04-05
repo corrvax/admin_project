c6fa882b00c058450b64b2be3d3f201d26f85f1b
김은조 <zmffheldk@gmail.com>
2022-04-06 01:20:23 +0900

    create OrderApiController

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -2,11 +2,14 @@ package com.example.study.controller.api;
 
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +20,7 @@ public class UserApiController implements CrudInterface {
 
     @Override
     @PostMapping("") // api/user
-    public Header create() {
+    public Header<UserApiResponse> create(@RequestBody UserApiRequest userApiRequest) {
         return null;
     }
 
