e1df96cded9e17a9e36ae7085580c9d19ccaaa17
김은조 <zmffheldk@gmail.com>
2022-04-06 00:50:27 +0900

    create result code

--- src/main/java/com/example/study/model/network/Header.java
+++ src/main/java/com/example/study/model/network/Header.java
@@ -1,5 +1,6 @@
 package com.example.study.model.network;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,12 +12,43 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Header<T> {
     //api 통신시간
-    private String transactionTime;
+    private LocalDateTime transactionTime;
     //api 응답 코드
     private String resultCode;
     //api 부가 설명
     private String description;
     //변경될 data
     private T data;
+
+    // result OK
+    public static <T> Header<T> OK(){
+        return (Header<T>) Header.builder()
+            .transactionTime(LocalDateTime.now())
+            .resultCode("OK")
+            .description("OK")
+            .build();
+
+    }
+
+    //data OK
+    public static <T> Header<T> OK(T data){
+        return (Header<T>) Header.builder()
+            .transactionTime(LocalDateTime.now())
+            .resultCode("OK")
+            .description("OK")
+            .data(data)
+            .build();
+
+    }
+
+    //ERROR
+    public static <T> Header<T> ERROR(String description){
+        return (Header<T>) Header.builder()
+            .transactionTime(LocalDateTime.now())
+            .resultCode("ERROR")
+            .description("description")//error reson
+            .build();
+
+    }
 
 }
