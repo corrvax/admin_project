70265ff25a281065004265b4aa332894bca7b509
김은조 <zmffheldk@gmail.com>
2022-04-06 00:40:21 +0900

    create CRUD interface

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -1,22 +1,32 @@
 package com.example.study.controller.api;
 
+import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController {
+public class UserApiController implements CrudInterface {
 
-//    public Header create(){     //C
-//
-//    }
-//
-//    public Header read(){       //R
-//
-//    }
-                                //U
 
-                                //D
+    @Override
+    public Header create() {
+        return null;
+    }
 
+    @Override
+    public Header read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header update() {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
 }
