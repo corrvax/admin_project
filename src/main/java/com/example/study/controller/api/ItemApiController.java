d9ac9ec0d9300556ce16875d1b221277d04ffa91
김은조 <zmffheldk@gmail.com>
2022-04-13 00:39:55 +0900

    create ItemApiController

--- src/main/java/com/example/study/controller/api/ItemApiController.java
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -1,7 +1,44 @@
 package com.example.study.controller.api;
 
-public class ItemApiController {
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-     
+@RestController
+@RequestMapping("/api/item")
+public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
 
+
+    @Override
+    @PostMapping("")        // /api/item
+    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("{id}")     // /api/item/100
+    public Header<ItemApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public Header delete(@PathVariable Long id) {
+        return null;
+    }
 }
